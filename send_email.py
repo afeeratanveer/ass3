a2518bf8b30809d489f9b8db0590e2dc9a573f71
Afeera <afeerat@gmail.com>
2023-05-02 17:52:38 +0500

    changes by afeera

--- send_email.py
+++ send_email.py
@@ -4,8 +4,8 @@ import os
 
 port = 465
 smtp_server = "smtp.gmail.com"
-USERNAME = os.environ.get('afeerat@gmail.com')
-PASSWORD = os.environ.get('twqkdtkormkculqz')
+USERNAME = os.environ.get('USER_EMAIL')
+PASSWORD = os.environ.get('USER_PASSWORD')
 message = """\
 Subject: GitHub Email Report
 
@@ -16,21 +16,4 @@ context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(USERNAME,PASSWORD)
     server.sendmail(USERNAME,USERNAME,message)
-import smtplib, ssl
-import os
 
-
-port = 465
-smtp_server = "smtp.gmail.com"
-USERNAME = os.environ.get('afeerat@gmail.com')
-PASSWORD = os.environ.get('twqkdtkormkculqz')
-message = """\
-Subject: GitHub Email Report
-
-This is your daily email report.
-"""
-
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(USERNAME,PASSWORD)
-    server.sendmail(USERNAME,USERNAME,message)
