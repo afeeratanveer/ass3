a361a43ef75488e7dfc26ebce94c7ca5e4e75b9c
jawaria2 <83430732+jawaria2@users.noreply.github.com>
2023-05-15 10:49:36 -0700

    Update IndexModelTests.cs

--- FirstAzureAppTestProject/IndexModelTests.cs
+++ FirstAzureAppTestProject/IndexModelTests.cs
@@ -1,60 +1,39 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstAzureWebApp.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace MyFirstAzureWebApp.Pages.Tests
 {
-    [TestClass()]
-    public class IndexModelTests
-    {
-        private readonly IndexModel _indexModel;
+	[TestClass()]
+	public class UnitTests_IndexModelTests
+	{
+		private readonly IndexModel _indexModel;
 
-        public IndexModelTests()
-        {
-            _indexModel = new IndexModel(null);
-        }
+		public UnitTests_IndexModelTests()
+		{
+			_indexModel = new IndexModel(null, null);
+		}
 
-        [TestMethod()]
-        public void AuthenticateTest_with_correct_credentials_results_successful()
-        {
+		[TestMethod]
+		public void Add_ByAddingTwoNumbers_returns_sum_of_numbers()
+		{
+			//Arrange
+			var a = 4;
+			var b = 5;
+			//Act
+			var result = _indexModel.add(a, b);
+			//Assert
+			Assert.AreEqual(9, result);
+		}
 
-            string userName = "test";
-            string password = "passcode";
-
-            var result = _indexModel.Authenticate(userName, password);
-
-            Assert.IsNotNull(result);
-            Assert.IsTrue(result.Equals($"Login succeeded for user {userName}"));
-
-            //Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void AuthenticateTest_when_I_Provide_wrong_credentials_it_should_fail_or_return_null()
-        {
-            //Assert.Fail();
-            string userName = "test";
-            string password = "passcode_abc!!";
-
-            var result = _indexModel.Authenticate(userName, password);
-
-            Assert.IsNull(result);
-
-        }
-
-        [TestMethod]
-        public void Add_ByAddingTwoNumbers_returns_sum_of_numbers()
-        {
-            //Arrange
-            var a = 4;
-            var b = 5;
-            //Act
-            var result = _indexModel.Add(a, b);
-            //Assert
-            Assert.AreEqual(9, result);
-        }
-
-        [TestMethod]
-        public void Add_ByAddingTwoNumbers_returns_sum_of_numbers_smart()
-        {
-            Assert.AreEqual(13, _indexModel.Add(6, 7));
-        }
-    }
+		[TestMethod]
+		public void Add_ByAddingTwoNumbers_returns_sum_of_numbers_smart()
+		{
+			Assert.AreEqual(12, _indexModel.add(6, 6));
+		}
+	}
 }
